e4215eb831a1cc62c42763a1f3ad770d2a41df0c
Adam Zolotarev <AZolotarev@covermymeds.com>
2018-02-01 13:43:26 -0500

    Moves files around

--- part1-data-preprocessing/data_preprocessing_template.py
+++ part1-data-preprocessing/data_preprocessing_template.py
@@ -1,19 +1,30 @@
 # Data Preprocessing Template
 
 # Importing the libraries
+# numpy is for math
 import numpy as np
+# chart plotting
 import matplotlib.pyplot as plt
+# to import and manage datasets
 import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
+# print(dataset)
+# take all rows, all columns except for the last
 X = dataset.iloc[:, :-1].values
+# take all rows, and only 3rd column
 y = dataset.iloc[:, 3].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
+# random_state set to 0 just so we have the same results as in the course
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=0)
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
--- part1-data-preprocessing/missing_data.py
+++ part1-data-preprocessing/missing_data.py
@@ -12,6 +12,9 @@ y = dataset.iloc[:, 3].values
 
 # Taking care of missing data
 from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+# axis 0 means mean along columns; axis 1 - across rows.
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean' , axis = 0)
 imputer = imputer.fit(X[:, 1:3])
+# take all columns; fix column 1 and 2 (upper is ecxluded)
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+print(X)
