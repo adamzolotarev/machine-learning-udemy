485dd270822a1739e5139c64ca169e0312b7d610
Adam Zolotarev <AZolotarev@covermymeds.com>
2018-02-12 14:14:24 -0500

    Adds backward elimination with p and R squared

--- part-2-regression/multiple_linear_regression/Homework_Solutions/multiple_linear_regression.py
+++ part-2-regression/multiple_linear_regression/Homework_Solutions/multiple_linear_regression.py
@@ -77,7 +77,39 @@ def backwardElimination(x, sl):
     return x
 
 
+def backwardEliminationWithAdjRSquared(x, SL):
+    numVars = len(x[0])
+    temp = np.zeros((50, 6)).astype(int)
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        adjR_before = regressor_OLS.rsquared_adj.astype(float)
+        if maxVar > SL:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    temp[:, j] = x[:, j]
+                    x = np.delete(x, j, 1)
+                    tmp_regressor = sm.OLS(y, x).fit()
+                    adjR_after = tmp_regressor.rsquared_adj.astype(float)
+                    if (adjR_before >= adjR_after):
+                        x_rollback = np.hstack((x, temp[:, [0, j]]))
+                        x_rollback = np.delete(x_rollback, j, 1)
+                        print(regressor_OLS.summary())
+                        return x_rollback
+                    else:
+                        continue
+    regressor_OLS.summary()
+    return x
+
+
+print('------------------------------------------------------')
+print('------------------------------------------------------')
+print('------------------------------------------------------')
+
 SL = 0.05
 X_opt = X[:, [0, 1, 2, 3, 4, 5]]
 X_Modeled = backwardElimination(X_opt, SL)
 print(X_Modeled)
+
+XModeledWithAdjRSquared = backwardEliminationWithAdjRSquared(X_opt, SL)
+print(XModeledWithAdjRSquared)
