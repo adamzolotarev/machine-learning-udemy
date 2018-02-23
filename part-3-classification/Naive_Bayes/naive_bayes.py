8257f48d892f549af34f250cd71d87817765a833
Adam Zolotarev <AZolotarev@covermymeds.com>
2018-02-23 15:24:03 -0500

    Adds Naive Bayes

--- part-3-classification/Naive_Bayes/naive_bayes.py
+++ part-3-classification/Naive_Bayes/naive_bayes.py
@@ -1,4 +1,4 @@
-# Support Vector Machine (SVM)
+# Naive Bayes
 
 # Importing the libraries
 import numpy as np
@@ -7,7 +7,7 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv(
-    '~/Dropbox/github/machine_learning_udemy/machine-learning-udemy/part-3-classification/SVM/Social_Network_Ads.csv')
+    '~/Dropbox/github/machine_learning_udemy/machine-learning-udemy/part-3-classification/Naive_Bayes/Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
@@ -22,9 +22,9 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Fitting SVM to the Training set
-from sklearn.svm import SVC
-classifier = SVC(kernel='linear', random_state=0)
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
@@ -34,6 +34,7 @@ y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
+
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
@@ -46,7 +47,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c=ListedColormap(('red', 'green'))(i), label=j)
-plt.title('SVM (Training set)')
+plt.title('Naive Bayes (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -64,7 +65,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c=ListedColormap(('red', 'green'))(i), label=j)
-plt.title('SVM (Test set)')
+plt.title('Naive Bayes (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
