b4718ecbdd953c148f674b0888abebecf8844efb
Adam Zolotarev <AZolotarev@covermymeds.com>
2018-04-06 08:57:41 -0400

    Adds natural language processing

---
+++ part-7-natural-language-processing/Natural_Language_Processing/natural_language_processing.py
@@ -0,0 +1,97 @@
+# Natural Language Processing
+
+
+# Stop Words:
+# ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she',"she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on','off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv(
+    '~/Dropbox/github/machine_learning_udemy/machine-learning-udemy/part-7-natural-language-processing/Natural_Language_Processing/Restaurant_Reviews.tsv', delimiter='\t', quoting=3)
+
+# Cleaning the texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+print(stopwords.words('english'))
+
+stop_words_set = set(stopwords.words('english'))
+
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    # stemming removes different version of the same word (love, loving, loves, loved... becomes love)
+    # stop words are the words which don't make difference to the algorithm(the, this, a, wow,...)
+    review = [ps.stem(word) for word in review if not word in stop_words_set]
+    review = ' '.join(review)
+    corpus.append(review)
+
+# Creating the Bag of Words model
+# Take all different but unique words and create one column for each word.
+# Then put in a table where the rows correspond to reviews, and columns to each word.
+# And in the cell is a number corresponding to how many times the word appears in a review.
+
+from sklearn.feature_extraction.text import CountVectorizer
+# take only top 1500 common words
+cv = CountVectorizer(max_features=1500)
+X = cv.fit_transform(corpus).toarray()
+# Look at X[0] vs corpus[0] here.
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.20, random_state=0)
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+print(y_pred)
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+#   0  1
+# 0
+# 1
+# (0,0) number of correct negative predictions
+# (1,1) number of correct positive predictions
+# (0,1) number of incorrect positive predictions
+# (1,0) number of incorrect negative predictions
+
+# 146 correct and 54 incorrect predictions ~73% accuracy - pretty good for 800 training set
+print(cm)
+
+# Accuracy = (TP + TN) / (TP + TN + FP + FN)
+# Precision = TP / (TP + FP)
+# Recall = TP / (TP + FN)
+# F1 Score = 2 * Precision * Recall / (Precision + Recall)
+true_positive = cm[0][0]
+true_negative = cm[1][1]
+false_positive = cm[0][1]
+false_negative = cm[1][0]
+
+accuracy = (true_positive + true_negative) / (true_positive +
+                                              true_negative + false_positive + false_negative)
+
+precision = true_positive / (true_positive + false_positive)
+
+recall = true_positive / (true_positive + false_negative)
+
+f1_score = 2 * precision * recall / (precision + recall)
+
+print("accuracy: ", accuracy)
+print("precision: ", precision)
+print("recall: ", recall)
+print("f1_score: ", f1_score)
